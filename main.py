75bc79cbac273b914a077f70bd8a97945ec20e63
Till Schnegelsberg <till.schnegelsberg@gmail.com>
2021-04-22 10:29:51 +0200

    fix: push script

--- main.py
+++ main.py
@@ -12,27 +12,43 @@ bot = commands.Bot(command_prefix='?')
 @bot.event
 async def on_ready():
     print("bot is ready for stuff")
+    await bot.change_presence(activity=discord.Game("lol"),afk=True)
 
 class general_stuff(commands.Cog):
 
     @commands.command()
+    async def ree(self, message):
+        print("lol")
+        await message.reply("ree")
+
+    @commands.command()
+    @commands.is_owner()
+    async def stop(self, message):
+        print("stopping")
+        await bot.logout()
+
+    @commands.command()
     async def update(self, message):
         cases.updateData()
-        embed = discord.Embed(title="Corona-Zahlen Landkreis Ludwigsburg",color=0x00ffff,timestamp=datetime.datetime.utcnow())
+        embed = discord.Embed(title="Cdddorona-Zahlen Landkreis Ludwigsburg", color=0x00ffff,
+                              timestamp=datetime.datetime.utcnow())
         embed.add_field(name="Korntal-Münchingen", value="Aktive Fälle: " + cases.getCasesIn("KorntalMuenchingen"),
                         inline=True)
         embed.add_field(name="Schwieberdingen", value="Aktive Fälle: " + cases.getCasesIn("Schwieberdingen"),
                         inline=True)
         embed.add_field(name="Hemmingen", value="Aktive Fälle: " + cases.getCasesIn("Hemmingen"), inline=True)
-        ratio = (float(cases.getValue("Tode_Gesamt"))*100 / int(cases.getValue("Faelle_Gesamt")))
+        ratio = (float(cases.getValue("Tode_Gesamt")) * 100 / int(cases.getValue("Faelle_Gesamt")))
         embed.add_field(name="Todesrate Ludwigsburg",
                         value=str(round(ratio, 2)) + "%",
                         inline=False)
         embed.add_field(name="Neue Fälle Deutschland (RKI)", value="+" + cases.getNewCasesInGermany(), inline=False)
         embed.add_field(name="Datenerhebungsdatum", value=cases.getValue("DashTime"), inline=False)
-        embed.set_footer(text=message.author.name + "#" + message.author.discriminator,icon_url=message.author.avatar_url)
-        embed.set_thumbnail(url="https://media.discordapp.net/attachments/464054727403896832/831636985897943080/303284178048211.png")
+        embed.set_footer(text=message.author.name + "#" + message.author.discriminator,
+                         icon_url=message.author.avatar_url)
+        embed.set_thumbnail(
+            url="https://media.discordapp.net/attachments/464054727403896832/831636985897943080/303284178048211.png")
         await message.reply(embed=embed)
+
 
 bot.add_cog(general_stuff())
 bot.run(os.environ["BOT_TOKEN"])
--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 docker build . -t t1il/covid-bot
 docker push t1il/covid-bot
-curl -H "Redeploy-Token: bvweiwqjdanfawjKL" jkoenig.dev:9000/hooks/redeploy-devmarkt
+curl -H "Redeploy-Token: bvweiwqjdanfawjKL" 192.168.178.66:9000/hooks/redeploy
+26ac0da0-a344-11eb-bcbc-0242ac130002
