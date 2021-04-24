7b931cb5eda2fadeb9e4e8b3bc5189bf6dd3c786
Till Schnegelsberg <till.schnegelsberg@gmail.com>
2021-04-24 14:00:20 +0200

    fix: redeploy

--- main.py
+++ main.py
@@ -32,7 +32,10 @@ class general_stuff(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def redeploy(self, message):
-        ip = socket.gethostbyname(socket.gethostname())
+        ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
+if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)),
+s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET,
+socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
         print(ip)
         requests.get("http:/" + ip + ":9000/hooks/redeploy")
 
